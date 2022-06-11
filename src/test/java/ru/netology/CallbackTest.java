f898a9160d13657bf39daeded38a46a22673b553
anna-mih <anedbalskaya@yandex.ru>
2022-06-11 08:23:28 +0300

    initial commit

---
+++ src/test/java/ru/netology/CallbackTest.java
@@ -0,0 +1,24 @@
+package ru.netology;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CallbackTest {
+    @Test
+    public void shouldSendFormSuccess() {
+        open("http://localhost:9999/");
+        SelenideElement form = $(".form");
+        form.$("[data-test-id=name] input").setValue("Екатерина Великая");
+        form.$("[data-test-id=phone] input").setValue("+79998887654");
+        form.$("[data-test-id=agreement]").click();
+        form.$("button").click();
+        String actual =  $("[data-test-id=order-success]").getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actual);
+    }
+}
